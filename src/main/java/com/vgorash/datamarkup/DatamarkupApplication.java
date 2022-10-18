111750a35dacd08331d68f3fd784eed3c50438b6
Vyacheslav Gorash <gorash590@gmail.com>
2022-10-18 18:58:40 +0300

    initial

---
+++ src/main/java/com/vgorash/datamarkup/DatamarkupApplication.java
@@ -0,0 +1,13 @@
+package com.vgorash.datamarkup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamarkupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatamarkupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vgorash/datamarkup/DatamarkupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vgorash.datamarkup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamarkupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
