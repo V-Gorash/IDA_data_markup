282292ed809853dbd89d385a1302074a8ae63dc2
Vyacheslav Gorash <gorash590@gmail.com>
2022-10-29 00:04:29 +0300

    image order

--- src/main/java/com/vgorash/datamarkup/repository/ImageRepository.java
+++ src/main/java/com/vgorash/datamarkup/repository/ImageRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT * FROM ida_image im WHERE im.id NOT IN (SELECT image_id from ida_mapping where user_username = ?1) LIMIT 1")
+    @Query(nativeQuery = true, value = "SELECT * FROM ida_image im" +
+            " WHERE im.id NOT IN (SELECT image_id from ida_mapping where user_username = ?1)" +
+            " order by (SELECT count(*) from ida_mapping mp where im.id = mp.image_id) LIMIT 1")
     Optional<Image> getImagesForUser(String username);
 }
